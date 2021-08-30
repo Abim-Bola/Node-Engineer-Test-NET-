dfb30ff70cff4345c7bb87c83c5c2c2b5c94b5ec
Abim-Bola <bamgbeluabimbolainfo@gmail.com>
2021-08-30 08:37:00 +0100

    work on migrations

--- database/migrations/1629123804886_transactions.ts
+++ database/migrations/1629123804886_transactions.ts
@@ -6,9 +6,9 @@ export default class Transactions extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.bigIncrements('id')
-      table.decimal('balanceBefore').notNullable()
-      table.decimal('balanceAfter').notNullable()
-      table.string('refrence').notNullable()
+      table.decimal('balance_before').notNullable()
+      table.decimal('balance_after').notNullable()
+      table.string('reference').notNullable()
       table.string('wallet_id').notNullable()
       table.enum('type', ['CREDIT', 'DEBIT']).notNullable()
       /**
